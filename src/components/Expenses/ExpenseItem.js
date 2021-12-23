2bfdd93a11638baeb742c355328dc7156db848a4
Kevin Benelli <kevinbenelli@Kevins-MacBook-Pro.local>
2021-12-22 19:01:14 -0800

    completed section 3

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,12 @@
+import React from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate
         title={props.title}
         amount={props.amount}
@@ -15,8 +18,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
