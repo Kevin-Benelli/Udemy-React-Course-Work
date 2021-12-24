6c1c4cdd0e93f181153dfcc3b1cc85e34dc87d27
Kevin Benelli <kevinbenelli@Kevins-MacBook-Pro.local>
2021-12-23 18:03:52 -0800

    cleaned code per instructions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
@@ -6,13 +6,6 @@ import Card from "../UI/Card";
 import "./ExpenseFilter";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("new title");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate
@@ -23,10 +16,9 @@ const ExpenseItem = (props) => {
 
       {/* <div> {props.date.toISOString()}</div> */}
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
